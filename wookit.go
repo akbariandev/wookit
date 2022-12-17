fa6680a9112cab734d818836641f23ddd621ed9d
mj <akbarian.dev@gmail.com>
2022-12-17 18:46:14 +0330

    feat: get product list

--- wookit.go
+++ wookit.go
@@ -5,18 +5,21 @@ import (
 	"github.com/akbariandev/wookit/src/coupon"
 	"github.com/akbariandev/wookit/src/customer"
 	"github.com/akbariandev/wookit/src/order"
+	"github.com/akbariandev/wookit/src/product"
 )
 
 type wooKitServices struct {
 	*coupon.CouponService
 	*order.OrderService
 	*customer.CustomerService
+	*product.ProductService
 }
 
 type WooKit interface {
 	coupon.ICoupon
 	order.IOrder
 	customer.ICustomer
+	product.IProduct
 }
 
 func NewWooKit(address, cs, ck string) WooKit {
@@ -30,5 +33,6 @@ func NewWooKit(address, cs, ck string) WooKit {
 		CouponService:   coupon.NewCouponService(Config),
 		OrderService:    order.NewOrderService(Config),
 		CustomerService: customer.NewCustomerService(Config),
+		ProductService:  product.NewProductService(Config),
 	}
 }
